8db74fbd21509f24c899abaef9965f9bbe2a5507
Y-Alomar <113862815+Y-Alomar@users.noreply.github.com>
2023-11-05 23:54:04 -0700

    Part I
    Setting up the files needed

---
+++ Game engine/Game engine.cpp
@@ -0,0 +1,11 @@
+// Game engine.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "olcConsoleGameEngine.h"
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    return 0;
+}
