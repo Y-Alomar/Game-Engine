60ca21ca87c92312fe7e030f13b83323d81699d0
Y-Alomar <113862815+Y-Alomar@users.noreply.github.com>
2023-11-06 00:33:04 -0700

    Continuing
    Just adding more stuff there is something wrong with the code probably some syntax issue

--- Game engine/Game engine.cpp
+++ Game engine/Game engine.cpp
@@ -2,10 +2,164 @@
 //
 
 #include "olcConsoleGameEngine.h"
+using namespace std;
+
+
+
+
+struct vec3d
+{
+    float x, y, z;
+};
+
+struct triangle
+{
+    vec3d p[3];
+};
+
+struct mesh
+{
+    vector<triangle> tris;
+};
+
+struct mat4x4
+{
+    float m[4][4] = { 0 };
+};
+
+
+
+
+
+
+
+
+class olcEngine3D : public olcConsoleGameEngine
+{
+public:
+    olcEngine3D()
+    {
+        m_sAppName = L"3D Demo";
+    }
+
+private:
+    mesh meshCube;
+    mat4x4 matProj;
+
+    void MultiplyMatrixVector(vec3d& i, vec3d& o, mat4x4& m)
+    {
+        o.x = i.x * m.m[0][0] + i.y * m.m[1][0] + i.z * m.m[2][0] + m.m[3][0];
+        o.y = i.x * m.m[0][1] + i.y * m.m[1][1] + i.z * m.m[2][1] + m.m[3][1];
+        o.z = i.x * m.m[0][2] + i.y * m.m[1][2] + i.z * m.m[2][2] + m.m[3][2];
+        float w = i.x * m.m[0][3] + i.y * m.m[1][3] + i.z * m.m[2][3] + m.m[3][3];
+
+        if (w != 0.0f) //Just to go back to 3D space
+        {
+            o.x /= w; o.y /= w; o.z /= w;
+        }
+    }
+
+
+
+
+
+public:
+    bool OnUserCreate() override
+    {
+        meshCube.tris = { //Initializer list
+        
+            
+        // SOUTH
+        { 0.0f, 0.0f, 0.0f,    0.0f, 1.0f, 0.0f,    1.0f, 1.0f, 0.0f }, //set of three points to make a triangle in each of these initalizer lists
+        { 0.0f, 0.0f, 0.0f,    1.0f, 1.0f, 0.0f,    1.0f, 0.0f, 0.0f },
+
+        // EAST                                                      
+        { 1.0f, 0.0f, 0.0f,    1.0f, 1.0f, 0.0f,    1.0f, 1.0f, 1.0f },
+        { 1.0f, 0.0f, 0.0f,    1.0f, 1.0f, 1.0f,    1.0f, 0.0f, 1.0f },
+
+        // NORTH                                                     
+        { 1.0f, 0.0f, 1.0f,    1.0f, 1.0f, 1.0f,    0.0f, 1.0f, 1.0f },
+        { 1.0f, 0.0f, 1.0f,    0.0f, 1.0f, 1.0f,    0.0f, 0.0f, 1.0f },
+
+        // WEST                                                      
+        { 0.0f, 0.0f, 1.0f,    0.0f, 1.0f, 1.0f,    0.0f, 1.0f, 0.0f },
+        { 0.0f, 0.0f, 1.0f,    0.0f, 1.0f, 0.0f,    0.0f, 0.0f, 0.0f },
+
+        // TOP                                                       
+        { 0.0f, 1.0f, 0.0f,    0.0f, 1.0f, 1.0f,    1.0f, 1.0f, 1.0f },
+        { 0.0f, 1.0f, 0.0f,    1.0f, 1.0f, 1.0f,    1.0f, 1.0f, 0.0f },
+
+        // BOTTOM                                                    
+        { 1.0f, 0.0f, 1.0f,    0.0f, 0.0f, 1.0f,    0.0f, 0.0f, 0.0f },
+        { 1.0f, 0.0f, 1.0f,    0.0f, 0.0f, 0.0f,    1.0f, 0.0f, 0.0f },
+
+        }
+
+        //Projection Matrix
+        float fNear = 0.1f;
+        float fFar = 1000.0f;
+        float fFov = 90.0f;
+        float fAspectRatio = (float)ScreenHeight() / (float)ScreenWidth;
+        float fFovRad = 1.0f / tanf(fFov * 0.5f / 180.0f * 3.14159f);
+
+        matProj.m[0][0] = fAspectRation * fFovRad;
+        matProj.m[1][1] = fFovRad;
+        matProj.m[2]2] = fFar / (fFar - fNear);
+        matProj.m[3][2] = (-fFar * fNear) / (fFar - fNear);
+        matProj.m[2][3] = 1.0f;
+        matProj.m[3][3] = 0.0f;
+
+        return true;
+    }
+    bool OnUserUpdate(float fElapsedTime) override
+    {
+        //Clear screen
+        Fill(0, 0, ScreenWidth(), ScreenHeight(), PIXEL_SOLID, FG_BLACK);
+
+        //Draw Triangles
+        for (auto tri : meshCube.tris)
+        {
+            triangle triProjected;
+            MultiplyMatrixVector(tri.p[0], triProject.p[0], matProj);
+            MultiplyMatrixVector(tri.p[1], triProject.p[1], matProj);
+            MultiplyMatrixVector(tri.p[2], triProject.p[2], matProj);
+
+            DrawTriangle(triProjected.p[0].x, triProjected.p[0].y,
+                triProjected.p[1].x, triProjected.p[1].y,
+                triProjected.p[2].x, triProjected.p[2].y,
+                PIXEL_SOLID, FG_WHITE);
+
+
+
+
+        }
+
+
+
+
+        return true;
+    }
+
+};
+
+
 
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    olcEngine3D demo;
+    if (demo.ConstructConsole(256, 240, 4, 4))
+    {
+        demo.Start();
+    }
+    else
+    {
+        cout << "Error" << endl;
+    }
+
+
+
+
+
     return 0;
 }
