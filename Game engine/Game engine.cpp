73b12b2127a5090cee1d6a54f0c8df0560a083df
Y-Alomar <113862815+Y-Alomar@users.noreply.github.com>
2023-11-13 11:12:31 -0700

    Need to fix somethimgs

--- Game engine/Game engine.cpp
+++ Game engine/Game engine.cpp
@@ -15,6 +15,9 @@ struct vec3d
 struct triangle
 {
     vec3d p[3];
+
+    wchar_t sym;
+    short col;
 };
 
 struct mesh
@@ -45,6 +48,11 @@ public:
 private:
     mesh meshCube;
     mat4x4 matProj;
+    vec3d vCamera;
+    
+
+
+
     float fTheta;
 
     void MultiplyMatrixVector(vec3d& i, vec3d& o, mat4x4& m)
@@ -59,12 +67,46 @@ private:
             o.x /= w; o.y /= w; o.z /= w;
         }
     }
+    // Taken From Command Line Webcam Video
+    CHAR_INFO GetColour(float lum)
+    {
+        short bg_col, fg_col;
+        wchar_t sym;
+        int pixel_bw = (int)(13.0f * lum);
+        switch (pixel_bw)
+        {
+        case 0: bg_col = BG_BLACK; fg_col = FG_BLACK; sym = PIXEL_SOLID; break;
+
+        case 1: bg_col = BG_BLACK; fg_col = FG_DARK_GREY; sym = PIXEL_QUARTER; break;
+        case 2: bg_col = BG_BLACK; fg_col = FG_DARK_GREY; sym = PIXEL_HALF; break;
+        case 3: bg_col = BG_BLACK; fg_col = FG_DARK_GREY; sym = PIXEL_THREEQUARTERS; break;
+        case 4: bg_col = BG_BLACK; fg_col = FG_DARK_GREY; sym = PIXEL_SOLID; break;
+
+        case 5: bg_col = BG_DARK_GREY; fg_col = FG_GREY; sym = PIXEL_QUARTER; break;
+        case 6: bg_col = BG_DARK_GREY; fg_col = FG_GREY; sym = PIXEL_HALF; break;
+        case 7: bg_col = BG_DARK_GREY; fg_col = FG_GREY; sym = PIXEL_THREEQUARTERS; break;
+        case 8: bg_col = BG_DARK_GREY; fg_col = FG_GREY; sym = PIXEL_SOLID; break;
+
+        case 9:  bg_col = BG_GREY; fg_col = FG_WHITE; sym = PIXEL_QUARTER; break;
+        case 10: bg_col = BG_GREY; fg_col = FG_WHITE; sym = PIXEL_HALF; break;
+        case 11: bg_col = BG_GREY; fg_col = FG_WHITE; sym = PIXEL_THREEQUARTERS; break;
+        case 12: bg_col = BG_GREY; fg_col = FG_WHITE; sym = PIXEL_SOLID; break;
+        default:
+            bg_col = BG_BLACK; fg_col = FG_BLACK; sym = PIXEL_SOLID;
+        }
+
+        CHAR_INFO c;
+        c.Attributes = bg_col | fg_col;
+        c.Char.UnicodeChar = sym;
+        return c;
+    }
 
 
 
 
 
 public:
+
     bool OnUserCreate() override
     {
         meshCube.tris = { //Initializer list
@@ -112,6 +154,10 @@ public:
 
         return true;
     };
+
+
+
+
     bool OnUserUpdate(float fElapsedTime) override
     {
         //Clear screen
@@ -185,11 +231,33 @@ public:
             float l = sqrtf(normal.x*normal.x + normal.y*normal.y + normal.z*normal.z);
             normal.x /= l; normal.y /= l; normal.z /= l;
 
-            if (normal.z < 0)
+            float dotProd = normal.x * (triTranslated.p[0].x - vCamera.x) +
+                normal.y * (triTranslated.p[0].y - vCamera.y) +
+                normal.z * (triTranslated.p[0].z - vCamera.z);
+
+
+
+            //if (normal.z < 0)
+            if(dotProd <0.0f)
             {
+                //illumination
+
+                vec3d light_direction = { 0.0f,0.0f,-1.0f };
+                float l = sqrtf(light_direction.x * light_direction.x + light_direction.y * light_direction.y + light_direction.z * light_direction.z);
+                light_direction.x /= l; light_direction.y /= l; light_direction.z /= l;
+
+                float dp = normal.x * light_direction.x + normal.y * light_direction.y + normal.z * light_direction * z;
+
+                CHAR_INFO c = GetColour(dp);
+                triTranslated.col = c.Attributes;
+                triTranslated.sym = c.Char.UnicodeChar;
+
+
                 MultiplyMatrixVector(triTranslated.p[0], triProjected.p[0], matProj);
                 MultiplyMatrixVector(triTranslated.p[1], triProjected.p[1], matProj);
                 MultiplyMatrixVector(triTranslated.p[2], triProjected.p[2], matProj);
+                triProjected.col = triTranslated.col;
+                triProjected.sym = triTranslated.sym;
 
                 //We need to scale into view as the range we are viewing from is [-1,1]
                 triProjected.p[0].x += 1.0f; triProjected.p[0].y += 1.0f;
@@ -206,7 +274,7 @@ public:
 
 
 
-                DrawTriangle(triProjected.p[0].x, triProjected.p[0].y,
+                FillTriangle(triProjected.p[0].x, triProjected.p[0].y,
                     triProjected.p[1].x, triProjected.p[1].y,
                     triProjected.p[2].x, triProjected.p[2].y,
                     PIXEL_SOLID, FG_WHITE);
